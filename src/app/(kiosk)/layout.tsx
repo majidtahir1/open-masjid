7e2f853c184738a3f7b9b90ce5f732ede99748f9
Majid Tahir <majid.tahir@catapulthealth.com>
2026-05-14 16:37:12 -0500

    fix(kiosk): wire Fraunces/Inter/Amiri font variables into kiosk layout (#75)

--- src/app/(kiosk)/layout.tsx
+++ src/app/(kiosk)/layout.tsx
@@ -1,9 +1,14 @@
 import '../globals.css'
 import './kiosk.css'
+import { fraunces, inter, amiri } from '@/lib/fonts'
+import { cn } from '@/lib/utils'
 
 export default function KioskLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={cn(fraunces.variable, inter.variable, amiri.variable)}
+    >
       <body
         style={{ margin: 0, background: '#000', color: '#fff', overflow: 'hidden' }}
       >
