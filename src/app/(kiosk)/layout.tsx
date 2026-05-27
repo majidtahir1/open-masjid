2f0ab0c37fe424a2bf5bc5dfd07d22708be945a3
Majid Tahir <majid.tahir@catapulthealth.com>
2026-05-27 00:39:23 -0500

    feat(kiosk): port prayer-display CSS + load Amiri Quran font

--- src/app/(kiosk)/layout.tsx
+++ src/app/(kiosk)/layout.tsx
@@ -1,13 +1,14 @@
 import '../globals.css'
 import './kiosk.css'
-import { fraunces, inter, amiri } from '@/lib/fonts'
+import './_components/prayer-display/prayer-display.css'
+import { fraunces, inter, amiri, amiriQuran } from '@/lib/fonts'
 import { cn } from '@/lib/utils'
 
 export default function KioskLayout({ children }: { children: React.ReactNode }) {
   return (
     <html
       lang="en"
-      className={cn(fraunces.variable, inter.variable, amiri.variable)}
+      className={cn(fraunces.variable, inter.variable, amiri.variable, amiriQuran.variable)}
     >
       <body
         style={{ margin: 0, background: '#000', color: '#fff', overflow: 'hidden' }}
